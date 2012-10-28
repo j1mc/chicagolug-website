83d90ff9e338c4f1850cb1a9103e77e120f7d466
Jim Campbell <jwcampbell@gmail.com>
2012-10-27 23:26:39 -0500

    added setup.py file to supply openshift dependencies.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='chicagolug-flask',
+      version='1.0',
+      description='ChicagoLUG website on OpenShift',
+      author='Jim Campbell',
+      author_email='jwcampbell@gmail.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+      install_requires=['flask>=0.9', 'markdown>=2.1.1', 'pyyaml>=3.10',
+      'gunicorn>=0.14.2'],
+     )
+
